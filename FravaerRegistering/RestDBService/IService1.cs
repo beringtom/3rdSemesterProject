e2568313f2e72f855c9f2385cca3b26177322eb4
Jesper Nørager Bay <jesper.bay@live.dk>
2017-11-28 14:20:13 +0100

    add rest service working

---
+++ FravaerRegistering/RestDBService/IService1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace RestDBService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        [WebInvoke(Method = "GET", RequestFormat = WebMessageFormat.Json, UriTemplate = "Person")]
+        IList<Person> GetAllPersons();
+
+        //[OperationContract]
+        //Person ReadPerson(IDataRecord reader);
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+
+    public class Login
+    {
+        [DataMember]
+        public int Login_Id;
+        [DataMember]
+        public string Login_Username;
+        [DataMember]
+        public string Login_Password;
+    }
+
+    public class Person
+    {
+        [DataMember]
+        public int Person_Id;
+        [DataMember]
+        public string Person_FirstName;
+        [DataMember]
+        public string Person_LastName;
+        [DataMember]
+        public string Person_Email;
+        [DataMember]
+        public string Person_StudentId;
+    }
+
+    public class Roles
+    {
+        [DataMember]
+        public int Roles_Id;
+        [DataMember]
+        public int Roles_Type;
+        [DataMember]
+        public string Roles_Name;
+    }
+
+    public class TimeRegistration
+    {
+        [DataMember]
+        public int TimeRegistration_Id;
+        [DataMember]
+        public DateTime TimeRegistration_CheckIn;
+        [DataMember]
+        public DateTime TimeRegistration_CheckOut;
+    }
+
+    public class Room
+    {
+        [DataMember]
+        public int Room_Id;
+        [DataMember]
+        public string Room_Name;
+    }
+
+    public class Team
+    {
+        [DataMember]
+        public int Team_Id;
+        [DataMember]
+        public string Team_Name;
+    }
+
+    // Schedule bliver sandsynligvis slettet igen, hvis den er unødvendig.
+    // Der skal mere research af Outlook til før vi ved det.
+
+    //public class Lecture
+    //{
+    //    [DataMember] public int Lecture_Id;
+    //    [DataMember] public DateTime Lecture_StartTime;
+    //    [DataMember] public DateTime Lecture_EndTime;
+    //}
+
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
---
+++ FravaerRegistering/RestDBService/Service1.svc.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace RestDBService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        private const string ConnectionString =
+                "Server=tcp:3rdsemesterprojectserver.database.windows.net,1433;Initial Catalog=3rdSemesterProjectDB;Persist Security Info=False;User ID=team4;Password=Noot1234;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+            ;
+
+        public IList<Person> GetAllPersons()
+        {
+            const string selectAllPersons = "select * from Person";
+
+            using (SqlConnection databaseConnection = new SqlConnection(ConnectionString))
+            {
+                databaseConnection.Open();
+                using (SqlCommand selectCommand = new SqlCommand(selectAllPersons, databaseConnection))
+                {
+                    using (SqlDataReader reader = selectCommand.ExecuteReader())
+                    {
+                        List<Person> studentList = new List<Person>();
+                        while (reader.Read())
+                        {
+                            Person student = ReadPerson(reader);
+                            studentList.Add(student);
+                        }
+                        return studentList;
+                    }
+                }
+            }
+        }
+
+        public Person ReadPerson(IDataRecord reader)
+        {
+            int id = reader.GetInt32(0);
+            string firstname = reader.GetString(1);
+            string lastname = reader.GetString(2);
+            string email = reader.GetString(3);
+            string studentid = reader.GetString(4);
+            Person person = new Person()
+            {
+                Person_Id = id,
+                Person_FirstName = firstname,
+                Person_LastName = lastname,
+                Person_Email = email,
+                Person_StudentId = studentid
+            };
+            return person;
+        }
+    }
+}
