f66edead36afc074bd007b1a0a05a09da00efb2d
fred3578 <fred3578@edu.easj.dk>
2017-12-01 14:27:33 +0100

    Added Addteam i interface

--- FravaerRegistering/RestDBService/IService1.cs
+++ FravaerRegistering/RestDBService/IService1.cs
@@ -37,6 +37,10 @@ namespace RestDBService
         [WebInvoke(Method = "DELETE", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, UriTemplate = "Person/{id}")]
         int DeletePerson(string id);
 
+        [OperationContract]
+        [WebInvoke(Method = "POST", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json,
+            UriTemplate = "Team/")]
+        void AddTeam(string teamName);
 
         //[OperationContract]
         //Person ReadPerson(IDataRecord reader);
