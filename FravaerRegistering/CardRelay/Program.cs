c43cd4f1858b1d1ecc9a4ad0aec857f8e67027ef
Ricco Mandahl Jørgensen <riccojorgensen@gmail.com>
2017-12-05 14:17:36 +0100

    Added JSON NUgget og lavet i CARDReader console program

--- FravaerRegistering/CardRelay/Program.cs
+++ FravaerRegistering/CardRelay/Program.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
 using System.Net.Sockets;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace CardRelay
 {
@@ -30,12 +33,18 @@ namespace CardRelay
                     string DecodedData = Encoding.ASCII.GetString(reciveData);
                     Console.WriteLine(DecodedData);
 
+                    string[] SplitDecodedData = DecodedData.Split('/');
+                    string Url = "restfravaerservice.azurewebsites.net/service1.svc/Sensor/";
 
-                    string SensorUrl = "";
+                    
+
+                    SensorData sdata = new SensorData(SplitDecodedData[1], DateTime.Parse(SplitDecodedData[2]), SplitDecodedData[3]);
+                    string response = AddSensordata(sdata ,Url).Result;
+
 
 
                     UdpClient Sender = new UdpClient(remote.Address.ToString(), ipout);
-                    byte[] send = Encoding.ASCII.GetBytes("CARD/" + "CHECKIN/" + DecodedData.Split('/')[2]);
+                    byte[] send = Encoding.ASCII.GetBytes("CARD/" + response + "/" + SplitDecodedData[3]);
                     Sender.Send(send, send.Length);
 
                 }
@@ -43,6 +52,27 @@ namespace CardRelay
                 {
                     Console.WriteLine(e.ToString());
                 }
+            }
+
+            
+        }
+
+
+
+
+        private static async Task<string> AddSensordata(object s, string uri)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                var jsonString = JsonConvert.SerializeObject(s);
+                StringContent content = new StringContent(jsonString, Encoding.UTF8, "application/json");
+
+
+                HttpResponseMessage response = await client.PostAsync(uri, content);
+                response.EnsureSuccessStatusCode();
+                string str = await response.Content.ReadAsStringAsync();
+                string returnString = JsonConvert.DeserializeObject<string>(str);
+                return returnString;
             }
         }
     }
---
+++ FravaerRegistering/CardRelay/SensorData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardRelay
+{
+    class SensorData
+    {
+        public string Room;
+        public DateTime Time;
+        public string CardID;
+
+        public SensorData(string room, DateTime time, string cardId)
+        {
+            Room = room;
+            Time = time;
+            CardID = cardId;
+        }
+    }
+}
